6cccd57608661ad8295d8dfaf25d9354884af9bc
codewithakshyaaa <akshyatiwary7@gmail.com>
2026-06-24 23:22:35 +0530

    fixedd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
+export default defineConfig(({ mode }) => {
+  // Yeh current environment ke variables load karega
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+    }
+  }
 })
